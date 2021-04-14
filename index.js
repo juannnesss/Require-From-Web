8c5158490645ba2243e96fdcc9888f9220c680cb
Juan Vargas <juan_vargas10@hotmail.com>
2021-04-14 13:19:53 -0500

    some test

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const axios = require('axios')
+"use strict";
+// https://github.com/TiagoDanin/Require-From-Web/blob/master/index.js
+// https://github.com/floatdrop/require-from-string/blob/master/index.js
 
-module.exports = async (url, options, code) => {
-	if (typeof url !== 'string') {
-		throw new TypeError('Expected a string')
-	}
+/* const Module = require("module")
+const path = require("path") */
+// code is BASE64 script based
+// file name is string filenaming
+module.exports = function requireFromString(code, filename, opts) {
+  /*   if (typeof filename === "object") {
+    opts = filename
+    filename = undefined
+  }
 
-	const response = await axios({
-		method: 'GET',
-		url,
-		...options
-	})
-	let string = response.data.toString()
-	if (code) {
-		string += '\n' + code
-	}
+  opts = opts || {}
+  filename = filename || ""
 
-	const _module = new module.constructor()
-	_module.filename = url
-	_module._compile(string, url)
+  opts.appendPaths = opts.appendPaths || []
+  opts.prependPaths = opts.prependPaths || [] */
 
-	return _module.exports
-}
+  if (typeof code !== "string") {
+    // eslint-disable-next-line unicorn/prefer-type-error
+    throw new Error("code must be a string, not " + typeof code);
+  }
+  let string = Buffer.from(code, "base64").toString();
+
+  let m = new module.constructor();
+  m.filename = filename;
+  m._compile(string, filename);
+
+  return module.exports;
+};
