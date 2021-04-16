320c51bcd01d072cf27bd2a7724de8901136a359
Juan Vargas <juan_vargas10@hotmail.com>
2021-04-16 15:50:29 -0500

    url capabilities

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 "use strict";
 // https://github.com/TiagoDanin/Require-From-Web/blob/master/index.js
 // https://github.com/floatdrop/require-from-string/blob/master/index.js
@@ -7,20 +8,29 @@ const path = require("path") */
 // code is BASE64 script based
 // file name is string filenaming
 module.exports = function requireFromString(code, filename, opts) {
-  /*   if (typeof filename === "object") {
-    opts = filename
-    filename = undefined
-  }
 
-  opts = opts || {}
-  filename = filename || ""
-
-  opts.appendPaths = opts.appendPaths || []
-  opts.prependPaths = opts.prependPaths || [] */
 
   if (typeof code !== "string") {
     // eslint-disable-next-line unicorn/prefer-type-error
     throw new Error("code must be a string, not " + typeof code);
+  }
+  // case when is base 64
+  if(code.includes("http")){
+    const response = await axios({
+      method: 'GET',
+      code,
+      ...opts
+    })
+    let string = response.data.toString()
+    if (code) {
+      string += '\n' + code
+    }
+  
+    const _module = new module.constructor()
+    _module.filename = url
+    _module._compile(string, url)
+  }else{
+
   }
   let string = Buffer.from(code, "base64").toString();
 
